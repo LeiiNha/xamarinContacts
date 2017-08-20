7274bf5e9d29114352f5281527b353e9e212b48f
Erica Geraldes <ericageraldes@hotmail.com>
2017-08-20 01:03:32 -0300

    Fix de solution

---
+++ Contacts/Contacts/Contacts/View/Camera.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Contacts.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Camera : ContentPage
+    {
+
+        public Func<Task> buttonCallback { get; set; }
+        public Camera()
+        {
+            InitializeComponent();
+            CameraButton.Clicked += CameraButton_Clicked;
+        }
+
+        private async void CameraButton_Clicked(object sender, EventArgs e)
+        {
+            await buttonCallback();
+            await Navigation.PopAsync();
+        }
+    }
+}
